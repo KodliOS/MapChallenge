a4f9c3e843040ae0392fb2d902a563d5530fa7bd
oguz <oguzveozturk@gmail.com>
2019-12-07 13:10:28 +0300

    Initialize the project

---
+++ MapChallenge/MapChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapChallenge
+//
+//  Created by Oguz on 7.12.2019.
+//  Copyright © 2019 Oguz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
