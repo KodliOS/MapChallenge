68efc5769297f96ed714262787172f6e35c8233a
oguz <oguzveozturk@gmail.com>
2019-12-09 15:03:31 +0300

    adding defaults google maps tab

--- MapChallenge/MapChallenge/AppleMapController.swift
+++ MapChallenge/MapChallenge/AppleMapController.swift
@@ -5,35 +5,23 @@
 //  Created by Oguz on 7.12.2019.
 //  Copyright © 2019 Oguz. All rights reserved.
 //
-
 import UIKit
-import CoreLocation
 import MapKit
 
 class AppleMapController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
-  
-   private var locationManager: CLLocationManager?
-    
+    private var locationManager: CLLocationManager?
     @IBOutlet weak var mapView: MKMapView!
-
-    let regionRadius: Double = 1000
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         locationManager = CLLocationManager()
         locationManager?.delegate = self
         locationManager?.requestAlwaysAuthorization()
         centerMapOnUserLocation()
-        
     }
-    
     func centerMapOnUserLocation() {
         guard let coordinate = locationManager?.location?.coordinate else {return}
-        let coordinateRegion = MKCoordinateRegion(center: coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        let coordinateRegion = MKCoordinateRegion(center: coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
         mapView.setRegion(coordinateRegion, animated: true)
-        
-        
     }
-    
 }
--- MapChallenge/MapChallenge/GoogleMapsController.swift
+++ MapChallenge/MapChallenge/GoogleMapsController.swift
@@ -5,25 +5,23 @@
 //  Created by Oguz on 7.12.2019.
 //  Copyright © 2019 Oguz. All rights reserved.
 //
-
 import UIKit
 import GoogleMaps
 
 class GoogleMapsController: UIViewController, CLLocationManagerDelegate  {
  private var locationManager: CLLocationManager?
     @IBOutlet weak var googleMapView: GMSMapView!
+    
     override func viewDidLoad() {
     super.viewDidLoad()
         locationManager = CLLocationManager()
         locationManager?.delegate = self
         locationManager?.requestWhenInUseAuthorization()
         locationManager?.startUpdatingLocation()
-        let camera = GMSCameraPosition.camera(withTarget: (locationManager?.location!.coordinate)!, zoom: 15)
+        guard let coordinate = locationManager?.location?.coordinate else {return}
+        let camera = GMSCameraPosition.camera(withTarget: coordinate, zoom: 15)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         view = mapView
         mapView.isMyLocationEnabled = true
-        
     }
-    
-
 }
